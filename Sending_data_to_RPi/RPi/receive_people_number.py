7603a5ad110b1fd2787f47e0d9cb7e37957227ad
ApinisAtvars <atvars.apinis@student.howest.be>
2024-06-07 16:46:59 +0200

    gave up on receiving data in json for now

--- Sending_data_to_RPi/RPi/receive_people_number.py
+++ Sending_data_to_RPi/RPi/receive_people_number.py
@@ -8,15 +8,19 @@ from threading import Thread
 import os
 import sys
 import csv
+import json
 
 # t = Thread(target=subprocess.run, args={"args": "cvlc v4l2:///dev/video0 --sout '#transcode{vcodec=h264,vb=1800,acodec=none}:rtp{sdp=rtsp://:8554/live.sdp}' :network-caching=50 Collapse", "shell": True})
 # t.start()
 
+#TODO HOW THE FUCK DO I RECEIVE THE FINAL DATA?!?!?!?!?!?!?
+
 previous_people_number = -1
 people_number = 0
 
-people_in = 0
-people_out = 0
+people_in = []
+people_out = []
+timestamps = []
 
 
 # Global vars for use in methods/threads
@@ -25,22 +29,6 @@ server_socket = None
 server_thread = None
 shutdown_flag = threading.Event() # see: https://docs.python.org/3/library/threading.html#event-objects
 
-# GPIO setup
-BUTTON_PIN = 7
-
-def button_callback(channel):
-    global client_socket
-    if client_socket: # if there is a connected client
-        try:
-            message = "Button Pressed!"
-            client_socket.sendall(message.encode()) # send a message
-        except:
-            print("Failed to send message")
-
-def setup_GPIO():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=200)
 
 def setup_socket_server():
     global server_socket, server_thread, shutdown_flag
@@ -70,24 +58,47 @@ def accept_connections(shutdown_flag):
 
 
 def handle_client(sock, shutdown_flag):
-    global previous_people_number, people_number
+    global previous_people_number, people_number, people_in, people_out, timestamps
     try:
-        while not shutdown_flag.is_set(): # as long as ctrl+c is not pressed
-            data = sock.recv(2) # try to receive 16 bytes
-            if not data: # when no data is received, try again (and shutdown flag is checked again)
-                # print("No data received")
-                continue # go back to top
-            people_number = data.decode()
-            print("Received from client:", people_number) # print the received data, or do something with it
-            
-            
-
+        # while not shutdown_flag.is_set(): # as long as ctrl+c is not pressed
+        #     data = sock.recv(3) # try to receive 3 bytes
+        #     if not data: # when no data is received, try again (and shutdown flag is checked again)
+        #         # print("No data received")
+        #         continue # go back to top
+        #     if "&" in data.decode():
+        #         while "$" not in data:
+        #             print(data)
+        #     people_number = data.decode()
+        #     print("Received from client:", people_number) # print the received data, or do something with it
+        while not shutdown_flag.is_set():
+            data = sock.recv(1024)
+            if not data:
+                continue
+            if data.decode() == "final_data":
+                final_data = sock.recv(1024).decode()
+                try:
+                    json_data = json.loads(final_data)
+                    people_in = json_data['people_in']
+                    people_out = json_data['people_out']
+                    timestamps = json_data['timestamps']
+                    print("Final data received")
+                    print("People In:", people_in)
+                    print("People Out:", people_out)
+                    print("Timestamps:", timestamps)
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
+                break
+            else:
+                people_number = int(data.decode())
+                print("Received from client:", people_number)
     except socket.timeout: # capture the timeouts 
         pass
     except Exception as e:
         print(f"Error: {e}")
     finally:
         sock.close()
+
+
 
 
 
@@ -110,7 +121,6 @@ def write_to_csv(number_of_people):
 ###### MAIN PART ######
 
 try:
-    setup_GPIO()
     setup_socket_server()
     lcd = LCD()
     lcd.display_text("Number of people", lcd.LCD_LINE_1)
@@ -131,7 +141,13 @@ except KeyboardInterrupt:
     print("Server shutting down")
     shutdown_flag.set() # set the shutdown flag
 finally:
-    server_thread.join() # join the thread, so we wait for it to finish (gracefull exit)
+    # server_thread.join() # join the thread, so we wait for it to finish (gracefull exit)
     server_socket.close() # make sure to close any open connections
-    GPIO.cleanup()
     lcd.clear_display()
+    lcd.turn_off()
+    print("LCD turned off")
+    GPIO.cleanup()
+    print("GPIO cleaned up")
+    print(people_in)
+    print(people_out)
+    print(timestamps)
